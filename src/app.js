fda3d1e54ad2f6c40ea64d142d0f3fe2e3b3db45
Rodrigo Botelho <tato.rodrigo@gmail.com>
2020-04-08 20:57:16 -0300

    implementação dos demais endpoints do desafio

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const {title, url, techs} = request.body;
+  const { title, url, techs } = request.body;
   const id = uuid();
   const likes = 0;
   const repository = {
@@ -23,22 +23,61 @@ app.post("/repositories", (request, response) => {
     title,
     url,
     techs,
-    likes
+    likes,
   };
   repositories.push(repository);
   return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  const repository = repositories[repositoryIndex];
+  const newRepository = Object.assign({}, { ...repository, title, url, techs });
+  repositories[repositoryIndex] = newRepository;
+
+  return response.json(newRepository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  const repository = repositories[repositoryIndex];
+  const newRepository = Object.assign({}, { ...repository, likes: repository.likes + 1 });
+  repositories[repositoryIndex] = newRepository;
+
+  return response.json(newRepository);
 });
 
 module.exports = app;
